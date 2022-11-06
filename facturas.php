39d852544f63d71a20f8854c7f4a6684fcfbc6cf
AlejandroCCCodes <110312076+AlejandroCCCodes@users.noreply.github.com>
2022-11-06 00:01:12 -0500

    Add files via upload

---
+++ facturas.php
@@ -0,0 +1,94 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<table class="default" align="center" width="100%">
+		<tr>
+			<td align="left" colspan="3"><img src="C:\Users\moren\Documents\Imagenes\Logo CFE.png"></td>
+			<td align="left" colspan="2"><font face="Arial Black" size="1">CFEM Suministrador de Servicios Basicos</font><font size="1"><br>Rio Rodano No.14, colonia Cuauhtemoc<br>Alcaldia Cuauhtemoc, Codigo Postal 06500,<br>Ciudad de México.<br>RFC: CSS160330CP7</font></td>
+		</tr>
+		<tr bgcolor="#EEEEEE">
+			<td colspan="3"><font size="5" face="Arial Black">CALZADA COLLAZOS, Leslie Lucero</font><br>Jr. Tarapaca 00S/N<br>Pblo PILCOMAYO<br>Pilcomayo, Huancayo - Junin<br><br></td>
+			<td colspan="2">TOTAL A PAGAR:<br><font size="5" face="Arial Black">$348.00</font><br>(TRESCIENTOS CUARENTA Y OCHO PESOS<br>00/100 M.N,)<br><br></td>
+		</tr>
+		<tr bgcolor="#EEEEEE">
+			<td colspan="3"><font size="2" face="Arial Black">RECIBO Nº  :</font> 811920400327<br><font size="2" face="Arial Black"></font>20129646099</td>
+			<td colspan="2"><font size="2" face="Arial Black">Recibo por Consumo del :</font>16 ENE 20 - 17 MAR 20</td>
+		</tr>
+		<tr bgcolor="#EEEEEE">
+			<td><font size="2" face="Arial Black">Consumo Total:</font></td>
+			<td><font size="2" face="Arial Black">Suministro:</font> 69038870</td>
+			<td><font size="2" face="Arial Black">MULTIPLICADOR:</font> 1</td>
+			<td><font size="2" face="Arial Black">Fecha de Vencimiento: 02 ABR 20</font></td>
+			<td><font size="2" face="Arial Black">Corte del Servicio:<br>03 ABR 20</font></td>
+		</tr>
+	</table>
+	<table class="default" align="center" width="75%">
+		<tr bgcolor="1DA200">
+			<th><font size="1" color="white">Departamento</font></th>
+			<th><font size="1" color="white">Lectura Anterior</font></th>
+			<th><font size="1" color="white">Lectura Actual</font></th>
+			<th><font size="1" color="white">Consumo 
+			<th colspan="2"><font size="2" color="white">Precio Total</font></th>
+		</tr>
+		<tr>
+			<td colspan="6"><font size="1"><br></font></td>
+		</tr>
+		<tr bgcolor="#EEEEEE">
+			<td><font size="1" face="Arial Black">PISO Nº 02</font></td>
+			<td align="right"><font size="1">01167</font></td>
+			<td align="right"><font size="1">00856</font></td>
+			<td><font size="1"><br></font></td>
+			<td align="left"><font size="1">311</font></td>
+			<td><font><br></font></td>
+		</tr>
+		<tr>
+			<td colspan="4" rowspan="7" align="center"><img src="C:/Users/moren/Documents/Imagenes/App CFE.jfif" width="250px" height="150px"><font size="5"></font></td>
+			<th bgcolor="#1DA200" colspan="2"><font size="1" color="white">NOVIEMBRE 2022</font></th>
+		</tr>
+		<tr>
+			<td colspan="2"><font size="1"><br></font></td>
+		</tr>
+		<tr bgcolor="#EEEEEE">
+			<td colspan="2"><font size="1"><br></font></td>
+		</tr>
+		<tr>
+			<td colspan="2"><font size="1"><br></font></td>
+		</tr>
+		<tr bgcolor="#EEEEEE">
+			<td colspan="2"><font size="1"><br></font></td>
+		</tr>
+		<tr>
+			<td colspan="2"><font size="1"><br></font></td>
+		</tr>
+		<tr bgcolor="#EEEEEE">
+			<td colspan="2"><font size="1"><br></font></td>
+		</tr>
+		<tr>
+			<td colspan="6" align="center"><br><br><img src="C:/Users/moren/Documents/Imagenes/Grafica de luz_04.jfif" width="100%" height="50px"></td>
+		</tr>
+	</table>
+	<table class="default" align="center" width="75%">
+		
+		<tr>
+			<td><font size="1"><br></font></td>
+			<td><font size="1"><br></font></td>
+			<td><font size="1"><br></font></td>
+			<td><font size="1"><br></font></td>
+			<td><font size="1"><br></font></td>
+			<td><font size="1"><br></font></td>
+			<td><font size="1"><br></font></td>
+		</tr>
+		<tr>
+		<td align="center"><img src="C:/Users/moren/Documents/Imagenes/QR.png" height="100px" width="100px"></td>
+		<td colspan="4" align="center"><img src="C:/Users/moren/Documents/Imagenes/Codigo de barras.png" height="100px"></td>
+		<td colspan="2"><font size="4" face="Arial Black">S/ ******* 348.00</font><br><font size="1">TOTAL A PAGAR</font></td>
+	    </tr>
+	    </table>
+		<input type="button" value="Imprime esta pagina" onclick="window.print()">
+</body>
+</html>
